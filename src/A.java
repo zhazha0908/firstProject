173b140437a0ba0d4e227e44c145ba3ae9a46bab
heqihang <heqihang@stu.cqut.edu.cn>
2022-09-30 10:49:10 +0800

    init

---
+++ src/A.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class A {
+    String sss;
+    int sssss;
+
+    public String getSss() {
+        return sss;
+    }
+
+    public void setSss(String sss) {
+        this.sss = sss;
+    }
+
+    public int getSssss() {
+        return sssss;
+    }
+
+    public void setSssss(int sssss) {
+        this.sssss = sssss;
+        System.out.println();
+        System.out.println(this.sssss);
+    }
+
+    public A(String sss, int sssss) {
+        this.sss = sss;
+        this.sssss = sssss;
+    }
+
+    public static void main(String[] args) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        LinkedList<Object> objects = new LinkedList<>();
+        System.out.println(objects);
+        for (int i = 0; i < 100; i++) {
+
+        }
+        for (int i = objects.size() - 1; i >= 0; i--) {
+
+        }
+    }
+}
